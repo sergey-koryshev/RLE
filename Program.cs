b6b4640a3e3c15ac157ac31550444b692a14149b
Ace-Lightning <acelightnin9@gmail.com>
2017-10-04 22:15:37 +0300

    Fix of Pack Method

--- Program.cs
+++ Program.cs
@@ -23,52 +23,46 @@ namespace RLE
         {
             for (int i = 0; i < count; i++)
             {
-                packedArray.Add(unpackedArray[index+i]);
+                packedArray.Add(unpackedArray[index + i]);
             }
         }
 
         static List<byte> Pack(List<byte> unpackedArray)
         {
-            List<byte> packedArray = new List<byte>();
-            int i = 0;
-            int count;
-
+            List<byte> packedArray = new List<byte>(); // массив запакованных данных
+            int i = 0; // позиция в "незапакованном" массиве
+            byte count; // количество "повторяющихся" элементов
+            int keyIndex = 0; // индекс ключевого байта
+            packedArray.Add(0); // добавляем первый ключевой байт
             while (i < unpackedArray.Count)
             {
-                if (i + 1 > unpackedArray.Count - 1) //если в массиве осталось только два элемента
+                count = 1; //пока у нас один элемент в "повторяющейся" последовательности
+                while (true) //цикл, чтобы сосчитать сколько повторяющихся элементов у нас есть
                 {
-                    InsertElements(packedArray, unpackedArray, i, 2); //то сжимать их не нужно, просто копируем
-                    i += 2;
+                    if (i + count > unpackedArray.Count - 1 || count >= 0x80) // необходимые условия на переполнение и на все остальное 
+                        break;
+                    if (unpackedArray[i] == unpackedArray[i + count])
+                    {
+                        count++;
+                    }
+                    else //если повторяющихся элементов больше нет, то выходим из цикла
+                        break;
                 }
-                else //но если в исходном массиве осталось больше двух элементов, то
+                if (count > 2) //если у нас последовательность потовряющихся элементов больше двух
                 {
-                    count = 1; //пока у нас один элемент в повторяющейся последовательности
-                    while (true) //цикл нужен для того, чтобы сосчитать сколько повторяющихся элементов у нас есть
-                    {
-                        if (i + count > unpackedArray.Count - 1 || count > 255)
-                            break;
-                        if (unpackedArray[i] == unpackedArray[i + count])
-                        {
-                            count++;
-                        }
-                        else //если повторяющихся элементов больше нет, то выходим из цикла
-                        {
-                            break;
-                        }
-                    }
-                    if (count > 2) //если у нас последовательность потовряющихся элементов больше двух
-                    {
-                        packedArray.Add((byte)count); //то записываем их количество
-                        packedArray.Add(unpackedArray[i]); //и записываем значение самого эелемента
-                        i += count; //продвигаемся по массиву дальше на длинну повторяющейся последовательности
-                    }
-                    else
-                    {
-                        InsertElements(packedArray, unpackedArray, i, count);
-                        i += count;
-                    }
+                    packedArray.Add(count); //то записываем их количество
+                    packedArray.Add(unpackedArray[i]); //и записываем значение самого эелемента
+                    packedArray[keyIndex] = count;
+                    packedArray.Add(0);
+                    keyIndex = packedArray.Count - 1;
                 }
+                else
+                {
+                    InsertElements(packedArray, unpackedArray, i, count); // если в последовательности два элемента и меньше, то просто последовательно записываем
+                }
+                i += count; //продвигаемся по массиву дальше на длинну повторяющейся последовательности
             }
+            packedArray.Add(255);
             return packedArray;
         }
     }
